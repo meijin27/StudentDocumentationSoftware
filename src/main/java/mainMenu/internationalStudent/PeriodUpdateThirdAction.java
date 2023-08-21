2b1efdfd14e20b22e62f564bf7aa7e6c74fea73e
meiji <meiji@AKINORI>
2023-08-21 17:41:39 +0900

    留学生用書類３枚目作成開始

---
+++ src/main/java/mainMenu/internationalStudent/PeriodUpdateThirdAction.java
@@ -0,0 +1,342 @@
+package mainMenu.internationalStudent;
+
+import java.text.NumberFormat;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+
+import dao.UserDAO;
+import tool.Action;
+import tool.CipherUtil;
+import tool.CustomLogger;
+import tool.Decrypt;
+import tool.DecryptionResult;
+import tool.EditPDF;
+
+public class PeriodUpdateThirdAction extends Action {
+	private static final Logger logger = CustomLogger.getLogger(PeriodUpdateThirdAction.class);
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// セッションの作成
+		HttpSession session = request.getSession();
+
+		// セッションの有効期限切れや直接初期設定入力ページにアクセスした場合はエラーとして処理
+		if (session.getAttribute("master_key") == null || session.getAttribute("id") == null) {
+			// ログインページにリダイレクト
+			session.setAttribute("otherError", "セッションエラーが発生しました。ログインしてください。");
+			String contextPath = request.getContextPath();
+			response.sendRedirect(contextPath + "/login/login.jsp");
+			return null;
+		}
+
+		// 入力された値を変数に格納
+
+		String relationship = request.getParameter("relationship");
+		String relationshipOtherContents = request.getParameter("relationshipOtherContents");
+		String foreignGovernment = request.getParameter("foreignGovernment");
+		String japaneseGovernment = request.getParameter("japaneseGovernment");
+		String localGovernment = request.getParameter("localGovernment");
+		String otherOrganization = request.getParameter("otherOrganization");
+		String publicInterest = request.getParameter("publicInterest");
+		String organizationpublicInterestContents = request.getParameter("organizationpublicInterestContents");
+		String organizationOtherContents = request.getParameter("organizationOtherContents");
+		String otherActivity = request.getParameter("otherActivity");
+		String work = request.getParameter("work");
+		String employment = request.getParameter("employment");
+		String workPhone = request.getParameter("workPhone");
+		String workTimePerWeek = request.getParameter("workTimePerWeek");
+		String salary = request.getParameter("salary");
+		String monthlyOrDaily = request.getParameter("monthlyOrDaily");
+		String afterGraduation = request.getParameter("afterGraduation");
+		String afterGraduationOtherContents = request.getParameter("afterGraduationOtherContents");
+
+		// 入力された値をリクエストに格納	
+		Enumeration<String> parameterNames = request.getParameterNames();
+		while (parameterNames.hasMoreElements()) {
+			String paramName = parameterNames.nextElement();
+			String paramValue = request.getParameter(paramName);
+			request.setAttribute(paramName, paramValue);
+		}
+
+		// 入力された金額をカンマ付きに変更するメソッド
+		NumberFormat numberFormat = NumberFormat.getNumberInstance();
+
+		// 申請人との関係がその他の場合で詳細記入がなければエラーを返す
+		if (relationship.equals("その他") && (relationshipOtherContents == null || relationshipOtherContents.isEmpty())) {
+			request.setAttribute("otherError", "申請人との関係でその他を選択した場合は詳細も入力してください。");
+		} else if (relationshipOtherContents.length() > 16) {
+			// 文字数が16文字より多い場合はエラーを返す
+			request.setAttribute("otherError", "その他の詳細は16文字以下で入力してください。");
+		}
+
+		// 奨学金支給機関が公益社団法人又は公益財団法人の場合で詳細記入がなければエラーを返す
+		if ((publicInterest != null && !publicInterest.isEmpty())
+				&& (organizationpublicInterestContents == null || organizationpublicInterestContents.isEmpty())) {
+			request.setAttribute("publicInterestError", "公益社団法人又は公益財団法人を選択した場合は詳細も入力してください。");
+		} else if (organizationpublicInterestContents.length() > 16) {
+			// 文字数が16文字より多い場合はエラーを返す
+			request.setAttribute("publicInterestError", "公益社団法人又は公益財団法人の詳細は16文字以下で入力してください。");
+		}
+
+		// 奨学金支給機関がその他の場合で詳細記入がなければエラーを返す
+		if ((otherOrganization != null && !otherOrganization.isEmpty())
+				&& (organizationOtherContents == null || organizationOtherContents.isEmpty())) {
+			request.setAttribute("otherOrganizationError", "奨学金支給機関でその他を選択した場合は詳細も入力してください。");
+		} else if (organizationOtherContents.length() > 16) {
+			// 文字数が16文字より多い場合はエラーを返す
+			request.setAttribute("otherOrganizationError", "その他の詳細は16文字以下で入力してください。");
+		}
+
+		// 更新項目確認用変数
+		boolean checkOtherActivity = false;
+
+		// 資格外活動の有無が有の場合で内容等が全て未入力なら問題なし
+		if (otherActivity.equals("有")) {
+			if ((work == null || work.isEmpty()) &&
+					(employment == null || employment.isEmpty()) &&
+					(workPhone == null || workPhone.isEmpty()) &&
+					(workTimePerWeek == null || workTimePerWeek.isEmpty()) &&
+					(salary == null || salary.isEmpty()) &&
+					(monthlyOrDaily == null || monthlyOrDaily.isEmpty())) {
+			} else if (work == null || work.isEmpty() ||
+					employment == null || employment.isEmpty() ||
+					workPhone == null || workPhone.isEmpty() ||
+					workTimePerWeek == null || workTimePerWeek.isEmpty() ||
+					salary == null || salary.isEmpty() ||
+					monthlyOrDaily == null || monthlyOrDaily.isEmpty()) {
+				// どれかだけ入力されている場合
+				request.setAttribute("otherActivityError", "資格外活動の内容を入力する場合は(1)～(4)の全項目を入力してください。");
+			} else if (work.length() > 32 || employment.length() > 32) {
+				// 文字数が32文字より多い場合はエラーを返す
+				request.setAttribute("otherActivityError", "32文字以下で入力してください。");
+			} else if (workPhone.matches("^\\d{10,11}$")) {
+				// 電話番号が半角10~11桁でなければエラーを返す
+				request.setAttribute("otherActivityError", "電話番号は半角数字10桁～11桁で入力してください。");
+			} else if (!workTimePerWeek.matches("\\d+") || workTimePerWeek.length() > 3 || !salary.matches("\\d+")
+					|| salary.length() > 7) {
+				// 稼働時間及び入力金額に数字以外が含まれている、もしくは数字が１０００万円を超える場合はエラーを返す
+				request.setAttribute("otherActivityError", "稼働時間は数字のみ3桁以下、報酬は数字のみ7桁以下で入力してください。");
+			} else {
+				/*
+				// 電話番号に「-」をつける
+				if (workPhone.length() == 11) {
+					workPhone = workPhone.substring(0, 3) + "-" + workPhone.substring(3, 7) + "-"
+							+ workPhone.substring(7, 11);
+				} else {
+					workPhone = workPhone.substring(0, 3) + "-" + workPhone.substring(3, 6) + "-"
+							+ workPhone.substring(6, 10);
+				}*/
+				// 文字列の数値をカンマ付きに変更する
+				salary = numberFormat.format(Integer.parseInt(salary));
+				checkOtherActivity = true;
+			}
+		}
+
+		// 卒業後の予定がその他の場合で詳細記入がなければエラーを返す
+		if (afterGraduation.equals("その他")
+				&& (afterGraduationOtherContents == null || afterGraduationOtherContents.isEmpty())) {
+			request.setAttribute("afterGraduationError", "卒業後の予定でその他を選択した場合は詳細も入力してください。");
+		} else if (afterGraduationOtherContents.length() > 32) {
+			// 文字数が21文字より多い場合はエラーを返す
+			request.setAttribute("afterGraduationError", "卒業後の予定のその他の詳細は32文字以下で入力してください。");
+		}
+
+		// エラーが発生している場合は元のページに戻す
+		if (request.getAttribute("otherError") != null || request.getAttribute("publicInterestError") != null
+				|| request.getAttribute("otherOrganizationError") != null
+				|| request.getAttribute("otherActivityError") != null
+				|| request.getAttribute("afterGraduationError") != null) {
+			return "period-update-third.jsp";
+		}
+
+		// リクエストのデータ全削除
+		Enumeration<String> attributeNames = request.getAttributeNames();
+		while (attributeNames.hasMoreElements()) {
+			String attributeName = attributeNames.nextElement();
+			request.removeAttribute(attributeName);
+		}
+
+		try {
+			// データベース操作用クラス
+			UserDAO dao = new UserDAO();
+			// 復号とIDやIV等の取り出しクラスの設定
+			Decrypt decrypt = new Decrypt(dao);
+			DecryptionResult result = decrypt.getDecryptedMasterKey(session);
+			// IDの取り出し
+			String id = result.getId();
+			// マスターキーの取り出し			
+			String masterKey = result.getMasterKey();
+			// ivの取り出し
+			String iv = result.getIv();
+
+			// 学生種類のデータベースからの取り出し
+			String reEncryptedStudentType = dao.getStudentType(id);
+			// 最初にデータベースから取り出したデータがnullの場合、初期設定をしていないためログインページにリダイレクト
+			if (reEncryptedStudentType == null) {
+				session.setAttribute("otherError", "初期設定が完了していません。ログインしてください。");
+				String contextPath = request.getContextPath();
+				response.sendRedirect(contextPath + "/login/login.jsp");
+				return null;
+			}
+			String encryptedStudentType = CipherUtil.commonDecrypt(reEncryptedStudentType);
+			String studentType = CipherUtil.decrypt(masterKey, iv, encryptedStudentType);
+			// もし学生種類が留学生でなければエラーを返す
+			if (!studentType.equals("留学生")) {
+				request.setAttribute("exchangeStudentError", "当該書類は留学生のみが発行可能です。");
+				return "period-update-third.jsp";
+			}
+
+			// PDFとフォントのパス作成
+			String pdfPath = "/pdf/internationalStudentPDF/在留期間更新許可申請書3枚目.pdf";
+			String fontPath = "/font/MS-Mincho-01.ttf";
+			// EditPDFのオブジェクト作成
+			EditPDF editor = new EditPDF(pdfPath);
+			// フォントの作成
+			PDFont font = PDType0Font.load(editor.getDocument(), this.getClass().getResourceAsStream(fontPath));
+			// PDFへの書き込み
+
+			// 申請人との関係	
+			switch (relationship) {
+			case "夫":
+				editor.writeText(font, "✓", 106f, 772f, 40f, "left", 8);
+				break;
+			case "妻":
+				editor.writeText(font, "✓", 145f, 772f, 40f, "left", 8);
+				break;
+			case "父":
+				editor.writeText(font, "✓", 184f, 772f, 40f, "left", 8);
+				break;
+			case "母":
+				editor.writeText(font, "✓", 223f, 772f, 40f, "left", 8);
+				break;
+			case "祖父":
+				editor.writeText(font, "✓", 262f, 772f, 40f, "left", 8);
+				break;
+			case "祖母":
+				editor.writeText(font, "✓", 314f, 772f, 40f, "left", 8);
+				break;
+			case "養父":
+				editor.writeText(font, "✓", 365f, 772f, 40f, "left", 8);
+				break;
+			case "養母":
+				editor.writeText(font, "✓", 417f, 772f, 40f, "left", 8);
+				break;
+			case "兄弟姉妹":
+				editor.writeText(font, "✓", 106f, 748f, 40f, "left", 8);
+				break;
+			case "叔父 （伯父）・叔母（伯母）":
+				editor.writeText(font, "✓", 184f, 748f, 40f, "left", 8);
+				break;
+			case "受入教育機関":
+				editor.writeText(font, "✓", 314f, 748f, 40f, "left", 8);
+				break;
+			case "友人・知人":
+				editor.writeText(font, "✓", 417f, 748f, 40f, "left", 8);
+				break;
+			case "友人・知人の親族":
+				editor.writeText(font, "✓", 106f, 725f, 40f, "left", 8);
+				break;
+			case "取引関係者・現地企業等職員":
+				editor.writeText(font, "✓", 210f, 725f, 40f, "left", 8);
+				break;
+			case "取引関係者・現地企業等職員の親族":
+				editor.writeText(font, "✓", 106f, 701f, 40f, "left", 8);
+				break;
+			case "その他":
+				editor.writeText(font, "✓", 314f, 701f, 40f, "left", 8);
+				editor.writeText(font, relationshipOtherContents, 360f, 701f, 82f, "center", 8);
+				break;
+			default:
+				break;
+			}
+
+			// 奨学金支給期間
+			// 外国政府
+			if (foreignGovernment != null && !foreignGovernment.isEmpty()) {
+				editor.writeText(font, "✓", 106f, 652f, 40f, "left", 8);
+			}
+
+			// 日本政府
+			if (japaneseGovernment != null && !japaneseGovernment.isEmpty()) {
+				editor.writeText(font, "✓", 184f, 652f, 40f, "left", 8);
+			}
+
+			// 地方公共団体
+			if (localGovernment != null && !localGovernment.isEmpty()) {
+				editor.writeText(font, "✓", 275f, 652f, 40f, "left", 8);
+			}
+			// 公益社団法人又は公益財団法人
+			if (publicInterest != null && !publicInterest.isEmpty()) {
+				editor.writeText(font, "✓", 106f, 628f, 40f, "left", 8);
+				editor.writeText(font, organizationpublicInterestContents, 255f, 628f, 95f, "center", 8);
+			}
+
+			// その他
+			if (otherOrganization != null && !otherOrganization.isEmpty()) {
+				editor.writeText(font, "✓", 366f, 628f, 40f, "left", 8);
+				editor.writeText(font, organizationOtherContents, 412f, 628f, 80f, "center", 8);
+			}
+
+			// 資格外活動の有無
+			if (otherActivity.equals("有")) {
+				editor.writeText(font, "〇", 440f, 592f, 40f, "left", 18);
+				if (checkOtherActivity) {
+					editor.writeText(font, work, 168f, 532f, 335f, "center", 12);
+					editor.writeText(font, employment, 168f, 508f, 155f, "center", 12);
+					editor.writeText(font, workPhone, 390f, 508f, 114f, "center", 12);
+					editor.writeText(font, workTimePerWeek, 168f, 485f, 50f, "center", 12);
+					editor.writeText(font, salary, 300f, 485f, 75f, "center", 12);
+					if (monthlyOrDaily.equals("月額")) {
+						editor.writeText(font, "✓", 404f, 495f, 40f, "left", 8);
+					} else {
+						editor.writeText(font, "✓", 450f, 495f, 40f, "left", 8);
+					}
+				}
+			} else {
+				editor.writeText(font, "〇", 461f, 592f, 40f, "left", 18);
+			}
+
+			// 申請人との関係	
+			switch (afterGraduation) {
+			case "帰国":
+				editor.writeText(font, "✓", 93f, 458f, 40f, "left", 8);
+				break;
+			case "日本での進学":
+				editor.writeText(font, "✓", 236f, 458f, 40f, "left", 8);
+				break;
+			case "日本での就職":
+				editor.writeText(font, "✓", 93f, 436f, 40f, "left", 8);
+				break;
+			case "その他":
+				editor.writeText(font, "✓", 236f, 436f, 40f, "left", 8);
+				editor.writeText(font, afterGraduationOtherContents, 283f, 436f, 210f, "center", 8);
+				break;
+			default:
+				break;
+			}
+
+			// Close and save
+			editor.close("在留期間更新許可申請書3枚目.pdf");
+			// 出力内容のデータベースへの登録
+			dao.addOperationLog(id, "Printing Period Update Third.jsp");
+			// PDF作成成功画面に遷移
+			request.setAttribute("createPDF",
+					"「在留期間更新許可申請書３枚目」を作成しました。在留期間更新許可申請書は３枚組で、当該書類は３枚目です。(The application form for permission to extend the period of stay is in triplicate, and the said document is the third one.)");
+			return "create-pdf-success-second.jsp";
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+			request.setAttribute("innerError", "内部エラーが発生しました。");
+			return "period-update-third.jsp";
+		}
+	}
+}
---
+++ src/main/webapp/mainMenu/internationalStudent/period-update-third.jsp
@@ -0,0 +1,203 @@
+<%@page contentType="text/html; charset=UTF-8" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/header/header-mainMenu.jsp" />
+
+<%@include file="/mainMenu/side-bar-menu.jsp" %>
+
+<main class="form-mainMenu col-md-9 ms-sm-auto col-lg-10  w-100 m-auto flex-shrink-0 px-md-4 mt-5">
+    <div class="justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-5 border-bottom">
+        <h1>「在留期間更新許可申請書　３枚目」作成</h1><br>
+    </div>			  
+		<form action="PeriodUpdateThird.action" method="post">
+   			<p class="text-start">・在留期間更新許可申請書は３枚組で、当該書類は３枚目です。</p>
+  			<p class="text-start margin-bottom-50  border-bottom">(The application form for permission to extend the period of stay is in triplicate, and the said document is the third one.)</p>
+	        <div class="row">
+ 	            <!-- 申請人との関係 -->
+      			<p class="text-start">申請人との関係 （2枚目で在外経費支弁者負担又は在日経費支弁者負担を選択した場合に記入）</p>
+				<p class="text-start">Relationship with the applicant (Check one of the followings when your answer to the question 22(1) is supporter living abroad or Japan)</p>
+	            <div class="col-md-12 mb-5">
+	                <select name="relationship" class="form-control select-center">
+	                    <option value="">-- 申請人との関係 --</option>
+	                    <option value="夫">夫（Husband）</option>
+	                    <option value="妻">妻（Wife）</option>
+	                    <option value="父">父（Father）</option>
+   	                    <option value="母">母（Mother）</option>
+   	                    <option value="祖父">祖父（Grandfather）</option>
+   	                    <option value="祖母">祖母（Grandmother）</option>
+   	                    <option value="養父">養父（Foster father）</option>
+   	                    <option value="養母">養母（Foster mother）</option>
+   	                    <option value="兄弟姉妹">兄弟姉妹（Brother / Sister）</option>
+   	                    <option value="叔父 （伯父）・叔母（伯母）">叔父 （伯父）・叔母（伯母）（Uncle / Aunt）</option>
+   	                    <option value="受入教育機関">受入教育機関（Educational institute）</option>
+   	                    <option value="友人・知人">友人・知人（Friend / Acquaintance）</option>
+   	                    <option value="友人・知人の親族">友人・知人の親族（Relative of friend / acquaintance）</option>
+   	                    <option value="取引関係者・現地企業等職員">取引関係者・現地企業等職員（Business connection / Personnel of local enterprise）</option>
+   	                    <option value="取引関係者・現地企業等職員の親族">取引関係者・現地企業等職員の親族（Relative of business connection / personnel of local enterprise）</option>
+   	                    <option value="その他">その他（Others）</option>
+	                </select>
+	            </div>	        
+ 	            <!-- その他の内容 -->
+      			<p class="text-start">その他を選択した場合は詳細記入（If you select "Others", please provide the following information）</p>
+	            <div class="col-md-12 mb-5">
+	                <input class="form-control" type="text" name="relationshipOtherContents" placeholder="" value="${relationshipOtherContents}">
+	            </div>   	            
+
+      			<p class="border-bottom"></p>
+ 	            <!-- 奨学金支給機関 -->
+       			<p class="text-start">奨学金支給機関 （2枚目でで奨学金を選択した場合に記入）※複数選択可</p>
+				<p class="text-start">Organization which provide scholarship (Check one of the following when the answer to the question 22(1) is scholarship)* multiple answers possible</p>
+				<div class="col-md-6 mb-3 d-flex align-items-center">
+				    <input class="form-check-input mt-0 mb-0" type="checkbox" id="foreignGovernment" name="foreignGovernment" value="foreignGovernment">
+				    <label for="foreignGovernment">外国政府（Foreign government）</label>
+				</div>
+				<div class="col-md-6 mb-3 d-flex align-items-center">
+				    <input class="form-check-input mt-0 mb-0" type="checkbox" id="japaneseGovernment" name="japaneseGovernment" value="japaneseGovernment">
+				    <label for="japaneseGovernment">日本国政府（Japanese government）</label>
+				</div>
+				<div class="col-md-6 mb-3 d-flex align-items-center">
+				    <input class="form-check-input mt-0 mb-0" type="checkbox" id="localGovernment" name="localGovernment" value="localGovernment">
+				    <label for="localGovernment">地方公共団体（Local government）</label>
+				</div>
+				<div class="col-md-6 mb-3 d-flex align-items-center">
+				    <input class="form-check-input mt-0 mb-0" type="checkbox" id="otherOrganization" name="otherOrganization" value="otherOrganization">
+				    <label for="otherOrganization">その他（Others）</label>
+				</div>
+				<div class="col-md-12 mb-5 d-flex align-items-center">
+				    <input class="form-check-input mt-0 mb-0" type="checkbox" id="publicInterest" name="publicInterest" value="publicInterest">
+				    <label for="publicInterest">公益社団法人又は公益財団法人（Public interest incorporated association /Public interest incorporated foundation）</label>
+				</div>
+				
+ 	            <!-- 公益社団法人又は公益財団法人の内容 -->
+      			<p class="text-start">公益社団法人又は公益財団法人を選択した場合は詳細記入（If you have selected a public interest incorporated association or public interest incorporated foundation, please provide details.）</p>
+	            <div class="col-md-12 mb-5">
+	                <input class="form-control" type="text" name="organizationpublicInterestContents" placeholder="" value="${organizationpublicInterestContents}">
+	            </div>   	       
+
+ 	            <!-- その他の内容 -->
+      			<p class="text-start">その他を選択した場合は詳細記入（If you select "Others", please provide the following information）</p>
+	            <div class="col-md-12 mb-5">
+	                <input class="form-control" type="text" name="organizationOtherContents" placeholder="" value="${organizationOtherContents}">
+	            </div>   	            
+      			
+       			<p class="border-bottom"></p>
+       			     			
+  	            <!-- 資格外活動の有無 -->
+  				<div class="col-md-12 mb-5">
+				    <label class="form-label">資格外活動の有無（Are you engaging in activities other than those permitted under the status of residence previously granted?）</label>
+	                <span class="text-danger">*</span>
+					<div class="d-flex align-items-center justify-content-center margin-bottom-20">
+					    <div class="form-check form-check-inline mr-lg-5">
+					        <input class="form-check-input" type="radio" name="otherActivity" id="otherActivityYes" value="有" 
+					               <% if ("有".equals(request.getParameter("otherActivity"))) { %> checked <% } %> required>
+					        <label class="form-check-label" for="otherActivityYes">
+					            有（Yes）
+					        </label>
+					    </div>
+					    <div class="form-check form-check-inline ml-lg-5">
+					        <input class="form-check-input" type="radio" name="otherActivity" id="otherActivityNo" value="無"
+					               <% if ("無".equals(request.getParameter("otherActivity"))) { %> checked <% } %>  required>
+					        <label class="form-check-label" for="otherActivityNo">
+					            無（No）
+					        </label>
+					    </div>
+					</div>
+				</div>            
+
+	  			<p class="text-start">資格外活動が有の場合は，(1)から(4)までの各欄を記入（複数ある場合は全て記入すること）※任意様式の別紙可</p> 
+ 	  			<p class="text-start">(Fill in (1) to (4) when your answer is "Yes".(Give the information for all of the companies if the applicant works for multiple companies)*another paper may be attached, which does not have to use a prescribed format.)</p> 
+ 	            <!-- 内容 -->
+	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="work">(1)内容（Type of work）</label>
+	                <input class="form-control" type="text" name="work" placeholder="レジ係" value="${work}">
+	            </div>
+   	            <!-- 勤務先名称 -->
+	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="employment">(2)勤務先名称（Place of employment）</label>
+	                <input class="form-control" type="text" name="employment" placeholder="〇〇コンビニ" value="${employment}">
+	            </div>
+   	            <!-- 勤務先電話番号 -->
+	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="workPhone">勤務先電話番号（Telephone No.）</label>
+	                <input class="form-control" type="text" name="workPhone" placeholder="046123456" value="${workPhone}">
+	            </div>
+   	            <!-- 週間稼働時間 -->
+	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="workTimePerWeek">(3)週間稼働時間（Work time per week）</label>
+	                <input class="form-control" type="text" name="workTimePerWeek" placeholder="15" value="${workTimePerWeek}">
+	            </div>
+   	            <!-- 報酬 -->
+	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="salary ">(4)報酬（Salary）</label>
+	                <input class="form-control" type="text" name="salary" placeholder="20000" value="${salary}">
+	            </div>	            
+  	            <!-- 月額か日額か -->
+   	            <div class="col-md-6 mb-5">
+	                <label class="form-label" for="monthlyOrDaily">月額・日額（Monthly or Daily）</label>
+	                <select name="monthlyOrDaily" class="form-control select-center">
+	                    <option value="">-- 月額・日額 --</option>
+	                    <option value="月額">月額（Monthly）</option>
+	                    <option value="日額">日額（Daily）</option>
+	                </select>
+	            </div>	
+	            
+       			<p class="border-bottom"></p>
+
+ 	            <!-- 卒業後の予定 -->
+	            <div class="col-md-12 mb-5">
+	                <label class="form-label" for="afterGraduation">卒業後の予定（Plan after graduation）</label>
+	                <span class="text-danger">*</span>
+	                <select name="afterGraduation" class="form-control select-center" required>
+	                    <option value="">-- 卒業後の予定 --</option>
+	                    <option value="帰国">帰国（Return to home country）</option>
+	                    <option value="日本での進学">日本での進学（Enter a school of higher education in Japan）</option>
+	                    <option value="日本での就職">日本での就職（Find work in Japan）</option>
+	                    <option value="その他">その他（Others）</option>
+	                </select>
+	            </div>	
+	            
+ 	            <!-- その他の内容 -->
+      			<p class="text-start">その他を選択した場合は詳細記入（If you select "Others", please provide the following information）</p>
+	            <div class="col-md-12 mb-5">
+	                <input class="form-control" type="text" name="afterGraduationOtherContents" placeholder="他国へ留学する" value="${afterGraduationOtherContents}">
+	            </div>   	    
+	            	            
+	        <c:if test="${not empty otherError}">
+	            <div class="alert alert-danger text-center input-field" role="alert">
+	                ${otherError}
+	            </div>
+	        </c:if>	        
+	        <c:if test="${not empty publicInterestError}">
+	            <div class="alert alert-danger text-center input-field" role="alert">
+	                ${publicInterestError}
+	            </div>
+	        </c:if>
+	        <c:if test="${not empty otherOrganizationError}">
+	            <div class="alert alert-danger text-center input-field" role="alert">
+	                ${otherOrganizationError}
+	            </div>
+	        </c:if>
+	        <c:if test="${not empty otherActivityError}">
+	            <div class="alert alert-danger text-center input-field" role="alert">
+	                ${otherActivityError}
+	            </div>
+	        </c:if>
+	        <c:if test="${not empty afterGraduationError}">
+	            <div class="alert alert-danger text-center input-field" role="alert">
+	                ${afterGraduationError}
+	            </div>
+	        </c:if>
+            <c:if test="${not empty exchangeStudentError}">
+            	<div class="alert alert-danger" role="alert">
+                	${exchangeStudentError}
+            	</div>
+        	</c:if>        	
+			<c:if test="${not empty innerError}">
+				<div class="alert alert-danger" role="alert">${innerError}
+				</div>
+			</c:if>
+			<button class="w-100 btn btn-lg btn-primary mb-3" id="submitButton" type="submit">作成</button>
+		</form>
+
+</main>
+
+<c:import url="/footer/footer-main-menu.jsp" />
